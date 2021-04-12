89a75b4e9b1ff1c77120ca6eb5e142a1ff66843e
Michayla Constantinou <michayla.constantinou@gmail.com>
2021-04-13 00:55:13 +1000

    added api JS file

--- script.js
+++ script.js
@@ -38,3 +38,13 @@ window.weatherWidgetConfig.push({
     showTitle:true, 
     showConditions:true
 });
+
+const express = require('express');
+const api = express();
+api.listen(3000, () => {
+  console.log('API up and running!');
+});
+api.get('/', (req, res) => {
+  console.log(req);
+  res.send('Hello, world!');
+});
